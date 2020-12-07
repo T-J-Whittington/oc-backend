17f32e018ce8b05b9ff2d1ead690238c55aba986
Thomas Whittington <thomasjackwhittington@gmail.com>
2020-12-07 02:15:38 +0000

    Original Cottages Test
    Creation of early elements.
    -Set up backend structure.
    -Created ordercontroller endpoint.
    --Attempted Symfony routing for endpoints.
    --getOrders requires testing.
    --getOrder and addOrder require completion.

---
+++ Controller/OrderController.php
@@ -0,0 +1,92 @@
+<?php
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderController
+{
+
+    private $db;
+
+    public function __construct()
+    {
+        $user = 'root';
+        $password = 'root';
+        $db = 'original_cottages';
+        $host = 'localhost';
+
+        $this->db = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+    }
+
+    /**
+     * Get a lightly detailed list of all orders
+     * @Route("/getOrders")
+     * @return array
+     */
+    public function getOrders()
+    {
+        $stmt = $this->db->prepare("
+            SELECT o.id,
+                   date,
+                   c.firstname || ' ' || c.lastname AS name,
+                   (
+                       SELECT SUM(i.price) AS total
+                       FROM order_details od
+                            INNER JOIN inventory i on od.item_id = i.id
+                       WHERE order_id = o.id
+                   ) AS total
+            FROM orders o
+                INNER JOIN customer c on o.customer = c.id
+            ");
+
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Get detailed information on a single order.
+     * @Route("/getOrder")
+     * @return array
+     */
+    public function getOrder()
+    {
+        $stmt = $this->db->prepare("
+            SELECT o.id,
+                   date,
+                   c.firstname || ' ' || c.lastname AS name,
+                   (
+                       SELECT SUM(i.price) AS total
+                       FROM order_details od
+                            INNER JOIN inventory i on od.item_id = i.id
+                       WHERE order_id = o.id
+                   ) AS total
+            FROM orders o
+                INNER JOIN customer c on o.customer = c.id
+            ");
+
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Add an order.
+     * @Route("/getOrder")
+     * @return string
+     */
+    public function addOrder()
+    {
+        $this->db->beginTransaction();
+        $stmt = $this->db->prepare("
+            INSERT INTO
+            ");
+        //TODO: finish the insert into and handle params.
+
+        try{
+            $stmt->execute();
+            $this->db->commit();
+            return "success";
+        } catch(Exception $e){
+            $this->db->rollBack();
+            return "failure";
+        }
+    }
+}
