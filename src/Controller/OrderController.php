6581d9d4ee9253602b79c68bc4ff7e09ad4eb827
Thomas Whittington <thomasjackwhittington@gmail.com>
2020-12-16 03:20:25 +0000

    Backend Development
    -Created endpoint contents for viewOrder.
    -Takes GET variable and returns contents.

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -61,22 +61,38 @@ class OrderController extends AbstractController
      */
     public function getOrder(Request $request)
     {
+        $requestData = $request->query->all();
+        $data['request'] = $requestData['id'];
         $stmt = $this->db->prepare("
             SELECT o.id,
-                   date,
-                   c.firstname || ' ' || c.lastname AS name,
-                   (
-                       SELECT SUM(i.price) AS total
-                       FROM order_details od
-                            INNER JOIN inventory i on od.item_id = i.id
-                       WHERE order_id = o.id
-                   ) AS total
+                   o.date,
+                   c.id AS custNum,
+                   c.firstname,
+                   c.lastname
             FROM orders o
                 INNER JOIN customer c on o.customer = c.id
+            WHERE o.id = :order_id
             ");
 
-        $stmt->execute();
-        $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        $stmt->execute([":order_id" => $requestData['id']]);
+        $data['info'] = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+
+        $stmt = $this->db->prepare("
+            SELECT i.id,
+                   i.item,
+                   COUNT(i.id) AS quantity,
+                   i.price,
+                   COUNT(i.id) * i.price AS total
+            FROM inventory i
+                INNER JOIN order_details o on o.item_id = i.id
+            WHERE o.order_id = :order_id
+            GROUP BY i.id, i.item, i.price
+            ");
+
+        $stmt->execute([':order_id' => $data['info']['id']]);
+        $data['items'] = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
         $response = new JsonResponse();
         $response->setContent(json_encode($data));
         $response->headers->set('Access-Control-Allow-Origin', '*');
